af24f2e24caeff2eec221d73a996364f15a8896e
joao-noleto13 <joao.costa@bandtec.com.br>
2021-11-10 22:58:03 -0300

    Alterei a tela quadras

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,18 @@ function ocultar_texto(){
 
  p.style.opacity = "0" 
 }
+
+
+function mostrar_texto_2(){
+
+  var texto = p_2.value;
+
+ p_2.style.opacity = "1" 
+}
+
+function ocultar_texto_2(){
+
+  var texto = p_2.value;
+
+ p_2.style.opacity = "0" 
+}
