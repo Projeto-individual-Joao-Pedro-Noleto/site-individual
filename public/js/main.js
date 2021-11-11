0da22ab03e35f78db28604f1d177db1c792fbc8a
joao-noleto13 <joao.costa@bandtec.com.br>
2021-11-10 22:48:23 -0300

    modificaçoes na tela quadras

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,13 @@
+function mostrar_texto(){
 
-    function validacao(){
-        var usuario = input_usuario.value.trimStart().trimEnd();
-        var senha = input_senha.value.trimStart().trimEnd();
-        var url = document.URL;
+  var texto = p.value;
 
-        if(usuario.length == 0){
-            alert("Campo usuario vazio")
-        }
-        else if (usuario.indexOf('@') == -1 ){
-            alert("Campo Invalido, falta @")
-          } 
-          else if (usuario.indexOf('.com') == -1 ){
-            alert("Campo Vazio, falta .com")
-          } 
-          else if (senha.length == 0 ){
-            alert("Campo Senha vazio")
-          } 
+ p.style.opacity = "1" 
+}
 
-        else {
-            formulario_login.action = url.replace('login.html', 'dashboard.html');
-        
-        }
-    }
+function ocultar_texto(){
+
+  var texto = p.value;
+
+ p.style.opacity = "0" 
+}
