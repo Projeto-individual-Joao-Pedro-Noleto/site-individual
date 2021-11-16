85f7a5d2391a8c46c5522c655a8c4dc22b04ecdf
joao-noleto13 <joao.costa@bandtec.com.br>
2021-11-15 22:49:41 -0300

    index alterado e criaçao da tela cadastro quadras

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
+
+/* Função msotrar texto */
+
 function mostrar_texto(){
 
   var texto = p.value;
@@ -25,4 +28,35 @@ function ocultar_texto_2(){
   var texto = p_2.value;
 
  p_2.style.opacity = "0" 
+}
+
+
+function mostrar_texto_3(){
+
+  var texto = p_3.value;
+
+ p_3.style.opacity = "1" 
+}
+
+function ocultar_texto_3(){
+
+  var texto = p_3.value;
+
+ p_3.style.opacity = "0" 
+}
+
+
+
+function mostrar_texto_4(){
+
+  var texto = p_4.value;
+
+ p_4.style.opacity = "1" 
+}
+
+function ocultar_texto_4(){
+
+  var texto = p_2.value;
+
+ p_4.style.opacity = "0" 
 }
