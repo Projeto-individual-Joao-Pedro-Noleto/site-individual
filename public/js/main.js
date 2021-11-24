711c2b342c55af6a3dbaf56fea07fa5037a41bf7
joao-noleto13 <joao.costa@bandtec.com.br>
2021-11-23 23:11:00 -0300

    css tela analise começado e novos containers adicionados no css quadra

--- public/js/main.js
+++ public/js/main.js
@@ -79,6 +79,38 @@ function ocultar_texto_5(){
  p_5.style.opacity = "0" 
 }
 
+
+
+
+function mostrar_texto_6(){
+
+  var texto = p_6.value;
+
+ p_6.style.opacity = "1" 
+}
+
+function ocultar_texto_6(){
+
+  var texto = p_6.value;
+
+ p_6.style.opacity = "0" 
+}
+
+
+function mostrar_texto_7(){
+
+  var texto = p_7.value;
+
+ p_7.style.opacity = "1" 
+}
+
+function ocultar_texto_7(){
+
+  var texto = p_7.value;
+
+ p_7.style.opacity = "0" 
+}
+
 /* quadras */
 
 
@@ -87,31 +119,45 @@ function ocultar_texto_5(){
 function zona_centro_quadras(){
 
 
+  var kaleman = kaleman_zs;
+  var trinta = trinta_zs;
+  var paco = paco_zs;
+  
+var ibira = ibira_centro;
+var major = vila_buarque_centro;
+var quadra_bras = bras_centro;
+var zilda = zilda_centro;
 
   botoes.style.display = "none";
+  h1_ola.style.display = "none";
 
-  kaleman_zs.style.display = "flex";
-  trinta_zs.style.display = "flex";
-  paco_zs.style.display = "flex";
-  ibira_centro.style.display = "flex";
+ 
+  ibira.style.display = "flex";
+  major.style.display = "flex";
+  zilda.style.display = "flex";
+  quadra_bras.style.display = "flex";
+  
 }
 
 
-function zona_centro_quadras(){
+function zona_sul_quadras(){
 
+  var kaleman = kaleman_zs;
+  var trinta = trinta_zs;
+  var paco = paco_zs;
 
   h1_ola.style.display = "none";
   botoes.style.display = "none";
 
-  kaleman_zs.style.display = "flex";
-  trinta_zs.style.display = "flex";
-  paco_zs.style.display = "flex";
+  kaleman.style.display = "flex";
+  trinta.style.display = "flex";
+  paco.style.display = "flex";
   ibira_centro.style.display = "flex";
 }
 
 
 
-function zona_centro_quadras(){
+function zona_norte_quadras(){
 
 
 
