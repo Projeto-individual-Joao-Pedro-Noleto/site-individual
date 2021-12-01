40e554f601bf54643e0c38226ef828ecba1d1499
joao-noleto13 <joao.costa@bandtec.com.br>
2021-11-30 21:23:47 -0300

    Caixa texto adicionada e api cadastro quadra funcionando

--- public/js/main.js
+++ public/js/main.js
@@ -1,424 +1,339 @@
 
 /* Função msotrar texto */
 
-function mostrar_texto(){
+function mostrar_texto() {
+    var texto = p.value;
 
-  var texto = p.value;
-
- p.style.opacity = "1";
-
-
+    p.style.opacity = '1';
 }
 
-function ocultar_texto(){
+function ocultar_texto() {
+    var texto = p.value;
 
-  var texto = p.value;
-
- p.style.opacity = "0" 
+    p.style.opacity = '0';
 }
 
+function mostrar_texto_2() {
+    var texto = p_2.value;
 
-function mostrar_texto_2(){
-
-  var texto = p_2.value;
-
- p_2.style.opacity = "1" 
+    p_2.style.opacity = '1';
 }
 
-function ocultar_texto_2(){
+function ocultar_texto_2() {
+    var texto = p_2.value;
 
-  var texto = p_2.value;
-
- p_2.style.opacity = "0" 
+    p_2.style.opacity = '0';
 }
 
+function mostrar_texto_3() {
+    var texto = p_3.value;
 
-function mostrar_texto_3(){
-
-  var texto = p_3.value;
-
- p_3.style.opacity = "1" 
+    p_3.style.opacity = '1';
 }
 
-function ocultar_texto_3(){
+function ocultar_texto_3() {
+    var texto = p_3.value;
 
-  var texto = p_3.value;
-
- p_3.style.opacity = "0" 
+    p_3.style.opacity = '0';
 }
 
+function mostrar_texto_4() {
+    var texto = p_4.value;
 
-
-function mostrar_texto_4(){
-
-  var texto = p_4.value;
-
- p_4.style.opacity = "1" 
+    p_4.style.opacity = '1';
 }
 
-function ocultar_texto_4(){
+function ocultar_texto_4() {
+    var texto = p_4.value;
 
-  var texto = p_4.value;
-
- p_4.style.opacity = "0" 
+    p_4.style.opacity = '0';
 }
 
+function mostrar_texto_5() {
+    var texto = p_5.value;
 
-
-function mostrar_texto_5(){
-
-  var texto = p_5.value;
-
- p_5.style.opacity = "1" 
+    p_5.style.opacity = '1';
 }
 
-function ocultar_texto_5(){
+function ocultar_texto_5() {
+    var texto = p_5.value;
 
-  var texto = p_5.value;
-
- p_5.style.opacity = "0" 
+    p_5.style.opacity = '0';
 }
 
+function mostrar_texto_6() {
+    var texto = p_6.value;
 
-
-
-function mostrar_texto_6(){
-
-  var texto = p_6.value;
-
- p_6.style.opacity = "1" 
+    p_6.style.opacity = '1';
 }
 
-function ocultar_texto_6(){
+function ocultar_texto_6() {
+    var texto = p_6.value;
 
-  var texto = p_6.value;
-
- p_6.style.opacity = "0" 
+    p_6.style.opacity = '0';
 }
 
+function mostrar_texto_7() {
+    var texto = p_7.value;
 
-function mostrar_texto_7(){
-
-  var texto = p_7.value;
-
- p_7.style.opacity = "1" 
+    p_7.style.opacity = '1';
 }
 
-function ocultar_texto_7(){
+function ocultar_texto_7() {
+    var texto = p_7.value;
 
-  var texto = p_7.value;
-
- p_7.style.opacity = "0" 
+    p_7.style.opacity = '0';
 }
 
-function mostrar_texto_8(){
+function mostrar_texto_8() {
+    var texto = p_8.value;
 
-  var texto = p_8.value;
-
- p_8.style.opacity = "1" 
+    p_8.style.opacity = '1';
 }
 
-function ocultar_texto_8(){
+function ocultar_texto_8() {
+    var texto = p_8.value;
 
-  var texto = p_8.value;
-
- p_8.style.opacity = "0" 
+    p_8.style.opacity = '0';
 }
 
+function mostrar_texto_9() {
+    var texto = p_9.value;
 
-function mostrar_texto_9(){
-
-  var texto = p_9.value;
-
- p_9.style.opacity = "1" 
+    p_9.style.opacity = '1';
 }
 
-function ocultar_texto_9(){
+function ocultar_texto_9() {
+    var texto = p_9.value;
 
-  var texto = p_9.value;
-
- p_9.style.opacity = "0" 
+    p_9.style.opacity = '0';
 }
 
-function mostrar_texto_10(){
+function mostrar_texto_10() {
+    var texto = p_10.value;
 
-  var texto = p_10.value;
-
- p_10.style.opacity = "1" 
+    p_10.style.opacity = '1';
 }
 
-function ocultar_texto_10(){
+function ocultar_texto_10() {
+    var texto = p_10.value;
 
-  var texto = p_10.value;
-
- p_10.style.opacity = "0" 
+    p_10.style.opacity = '0';
 }
 
-function mostrar_texto_11(){
+function mostrar_texto_11() {
+    var texto = p_11.value;
 
-  var texto = p_11.value;
-
- p_11.style.opacity = "1" 
+    p_11.style.opacity = '1';
 }
 
-function ocultar_texto_11(){
+function ocultar_texto_11() {
+    var texto = p_11.value;
 
-  var texto = p_11.value;
-
- p_11.style.opacity = "0" 
+    p_11.style.opacity = '0';
 }
 
+function mostrar_texto_12() {
+    var texto = p_12.value;
 
-
-function mostrar_texto_12(){
-
-  var texto = p_12.value;
-
- p_12.style.opacity = "1" 
+    p_12.style.opacity = '1';
 }
 
-function ocultar_texto_12(){
+function ocultar_texto_12() {
+    var texto = p_12.value;
 
-  var texto = p_12.value;
-
- p_12.style.opacity = "0" 
+    p_12.style.opacity = '0';
 }
 
-function mostrar_texto_13(){
+function mostrar_texto_13() {
+    var texto = p_13.value;
 
-  var texto = p_13.value;
-
- p_13.style.opacity = "1" 
+    p_13.style.opacity = '1';
 }
 
-function ocultar_texto_13(){
+function ocultar_texto_13() {
+    var texto = p_13.value;
 
-  var texto = p_13.value;
-
- p_13.style.opacity = "0" 
+    p_13.style.opacity = '0';
 }
 
-function mostrar_texto_14(){
+function mostrar_texto_14() {
+    var texto = p_14.value;
 
-  var texto = p_14.value;
-
- p_14.style.opacity = "1" 
+    p_14.style.opacity = '1';
 }
 
-function ocultar_texto_14(){
+function ocultar_texto_14() {
+    var texto = p_14.value;
 
-  var texto = p_14.value;
-
- p_14.style.opacity = "0" 
+    p_14.style.opacity = '0';
 }
 
-function mostrar_texto_15(){
+function mostrar_texto_15() {
+    var texto = p_15.value;
 
-  var texto = p_15.value;
-
- p_15.style.opacity = "1" 
+    p_15.style.opacity = '1';
 }
 
-function ocultar_texto_15(){
+function ocultar_texto_15() {
+    var texto = p_15.value;
 
-  var texto = p_15.value;
-
- p_15.style.opacity = "0" 
+    p_15.style.opacity = '0';
 }
 
+function mostrar_texto_16() {
+    var texto = p_16.value;
 
-function mostrar_texto_16(){
-
-  var texto = p_16.value;
-
- p_16.style.opacity = "1" 
+    p_16.style.opacity = '1';
 }
 
-function ocultar_texto_16(){
+function ocultar_texto_16() {
+    var texto = p_16.value;
 
-  var texto = p_16.value;
-
- p_16.style.opacity = "0" 
+    p_16.style.opacity = '0';
 }
 
+function mostrar_texto_17() {
+    var texto = p_17.value;
 
-function mostrar_texto_17(){
-
-  var texto = p_17.value;
-
- p_17.style.opacity = "1" 
+    p_17.style.opacity = '1';
 }
 
-function ocultar_texto_17(){
+function ocultar_texto_17() {
+    var texto = p_17.value;
 
-  var texto = p_17.value;
-
- p_17.style.opacity = "0" 
+    p_17.style.opacity = '0';
 }
 
-function mostrar_texto_18(){
+function mostrar_texto_18() {
+    var texto = p_17.value;
 
-  var texto = p_17.value;
-
- p_18.style.opacity = "1" 
+    p_18.style.opacity = '1';
 }
 
-function ocultar_texto_18(){
+function ocultar_texto_18() {
+    var texto = p_17.value;
 
-  var texto = p_17.value;
-
- p_18.style.opacity = "0" 
+    p_18.style.opacity = '0';
 }
-
-
 
 /* quadras */
 
+function zona_centro_quadras() {
+    var kaleman = kaleman_zs;
+    var trinta = trinta_zs;
+    var paco = paco_zs;
 
+    var sessao_quadra = tela_quadra;
+    var ibira = ibira_centro;
+    var major = vila_buarque_centro;
+    var quadra_bras = bras_centro;
+    var zilda = zilda_centro;
 
+    botoes_2.style.display = 'none';
+    botoes.style.display = 'none';
+    h1_ola.style.display = 'none';
 
-function zona_centro_quadras(){
+    bem_vindo.style.display = 'none';
+    tamanho_body.style.width = '200vw';
 
-
-  var kaleman = kaleman_zs;
-  var trinta = trinta_zs;
-  var paco = paco_zs;
-
-
-var sessao_quadra = tela_quadra;
-var ibira = ibira_centro;
-var major = vila_buarque_centro;
-var quadra_bras = bras_centro;
-var zilda = zilda_centro;
-
-  botoes_2.style.display = "none";
-  botoes.style.display = "none";
-  h1_ola.style.display = "none";
-
-  bem_vindo.style.display = "none"  
-  tamanho_body.style.width = "200vw"
-  
-  sessao_quadra.style.display = "flex";
-  ibira.style.display = "flex";
-  major.style.display = "flex";
-  zilda.style.display = "flex";
-  quadra_bras.style.display = "flex";
-  
+    sessao_quadra.style.display = 'flex';
+    ibira.style.display = 'flex';
+    major.style.display = 'flex';
+    zilda.style.display = 'flex';
+    quadra_bras.style.display = 'flex';
 }
 
+function zona_sul_quadras() {
+    var kaleman = kaleman_zs;
+    var trinta = trinta_zs;
+    var paco = paco_zs;
+    var uniao = uniao_zs;
 
-function zona_sul_quadras(){
+    var sessao_quadra = tela_quadra;
 
-  
+    h1_ola.style.display = 'none';
+    botoes.style.display = 'none';
+    botoes_2.style.display = 'none';
 
+    bem_vindo.style.display = 'none';
+    tamanho_body.style.width = '200vw';
+    sessao_quadra.style.display = 'flex';
 
-  var kaleman = kaleman_zs;
-  var trinta = trinta_zs;
-  var paco = paco_zs;
-  var uniao = uniao_zs;
-
-  var sessao_quadra = tela_quadra;
-
-  h1_ola.style.display = "none";
-  botoes.style.display = "none";
-  botoes_2.style.display = "none";
-
-  bem_vindo.style.display = "none"  
-  tamanho_body.style.width = "200vw"
-  sessao_quadra.style.display = "flex";
-
-  kaleman.style.display = "flex";
-  trinta.style.display = "flex";
-  paco.style.display = "flex";
-  uniao.style.display = "flex";
+    kaleman.style.display = 'flex';
+    trinta.style.display = 'flex';
+    paco.style.display = 'flex';
+    uniao.style.display = 'flex';
 }
 
+function zona_norte_quadras() {
+    var juventude = juventude_zn;
+    var jardim_felici = jardim_zn;
+    var lions = lions_zn;
+    var tiquatira = tiquatira_zn;
+    var sessao_quadra = tela_quadra;
+    var jacinto = jacinto_zn;
 
+    h1_ola.style.display = 'none';
+    botoes.style.display = 'none';
+    botoes_2.style.display = 'none';
 
-function zona_norte_quadras(){
+    bem_vindo.style.display = 'none';
+    tamanho_body.style.width = '200vw';
+    sessao_quadra.style.display = 'flex';
 
-
-  
-  var juventude = juventude_zn
-  var jardim_felici = jardim_zn;
-  var lions = lions_zn
-  var tiquatira = tiquatira_zn
-  var sessao_quadra = tela_quadra;
-  var jacinto = jacinto_zn
-
-  h1_ola.style.display = "none";
-  botoes.style.display = "none";
-  botoes_2.style.display = "none";
-
-  bem_vindo.style.display = "none"  
-  tamanho_body.style.width = "200vw"
-  sessao_quadra.style.display = "flex";
-
-  juventude.style.display = "flex";
-  jacinto.style.display = "flex";
-  lions.style.display = "flex";
-  jardim_felici.style.display = "flex";
+    juventude.style.display = 'flex';
+    jacinto.style.display = 'flex';
+    lions.style.display = 'flex';
+    jardim_felici.style.display = 'flex';
 }
 
+function zona_leste_quadras() {
+    var raul = raul_zl;
+    var ciencia = ciencia_zl;
+    var ceret = ceret_zl;
+    var tiquatira = tiquatira_zn;
+    var sessao_quadra = tela_quadra;
 
-function zona_leste_quadras(){
+    h1_ola.style.display = 'none';
+    botoes.style.display = 'none';
+    botoes_2.style.display = 'none';
 
+    bem_vindo.style.display = 'none';
+    tamanho_body.style.width = '200vw';
+    sessao_quadra.style.display = 'flex';
 
-  var raul = raul_zl;
-  var ciencia = ciencia_zl;
-  var ceret = ceret_zl;
-  var tiquatira = tiquatira_zn
-  var sessao_quadra = tela_quadra;
- 
-
-  h1_ola.style.display = "none";
-  botoes.style.display = "none";
-  botoes_2.style.display = "none";
-
-  bem_vindo.style.display = "none"  
-  tamanho_body.style.width = "200vw"
-  sessao_quadra.style.display = "flex";
-
-  raul.style.display = "flex";
-  tiquatira.style.display = "flex";
-  ceret.style.display = "flex";
-  ciencia.style.display = "flex";
+    raul.style.display = 'flex';
+    tiquatira.style.display = 'flex';
+    ceret.style.display = 'flex';
+    ciencia.style.display = 'flex';
 }
 
-function zona_oeste_quadras(){
+function zona_oeste_quadras() {
+    var villa = villa_zo;
+    var candido = candido_zo;
+    var sessao_quadra = tela_quadra;
 
+    h1_ola.style.display = 'none';
+    botoes.style.display = 'none';
+    botoes_2.style.display = 'none';
 
-  var villa = villa_zo;
-  var candido = candido_zo;
-  var sessao_quadra = tela_quadra;
- 
+    bem_vindo.style.display = 'none';
+    tamanho_body.style.width = '200vw';
+    sessao_quadra.style.display = 'flex';
 
-  h1_ola.style.display = "none";
-  botoes.style.display = "none";
-  botoes_2.style.display = "none";
-
-  bem_vindo.style.display = "none"  
-  tamanho_body.style.width = "200vw"
-  sessao_quadra.style.display = "flex";
-
-  candido.style.display = "flex";
-  villa.style.display = "flex";
-
+    candido.style.display = 'flex';
+    villa.style.display = 'flex';
 }
-
-
-
 
 /* Pegar dados do banco */
 
-function validarsessao(){
-  var nome = sessionStorage.NOME_USUARIO;
+function validarsessao() {
+    var nome = sessionStorage.NOME_USUARIO;
 
-  if( nome != null ){
-    nome_usuario_h1.innerHTML = nome;
-  }
-  else{
-    window.location = "../login.html"
-  }
+    if (nome != null) {
+        nome_usuario_h1.innerHTML = nome;
+    } else {
+        window.location = '../login.html';
+    }
 }
-
